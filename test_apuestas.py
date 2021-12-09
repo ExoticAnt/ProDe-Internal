f8dc23628478fd42b63a334ef225c427a0f381c9
Juan M. Sardón <juan.manuel.sardon@gmail.com>
2021-12-08 22:16:30 -0300

    Add files via upload

---
+++ test_apuestas.py
@@ -0,0 +1,15 @@
+import sys
+
+def application(environ,start_response):
+    status = '200 OK'
+    html = '<html>\n' \
+           '<body>\n' \
+           '<div style="width: 100%; font-size: 40px; font-weight: bold; text-align: center;">\n' \
+           'mod_wsgi Test Page\n Python Version: {0}' \
+           '</div>\n' \
+           '</body>\n' \
+           '</html>\n'
+    response_header = [('Content-type','text/html')]
+    start_response(status,response_header)
+    return [html.format(sys.version)]
+
